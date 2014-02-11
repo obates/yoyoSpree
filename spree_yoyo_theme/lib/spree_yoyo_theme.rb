596318c48c572ce955c675ed11d7e1b9191617a7
Oli Bates <olibates@live.com>
2014-02-11 12:38:30 +0000

    Initial rails/spree/theme commit

---
+++ spree_yoyo_theme/lib/spree_yoyo_theme.rb
@@ -0,0 +1,2 @@
+require 'spree_core'
+require 'spree_yoyo_theme/engine'
---
+++ yoyoburger/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yoyoburger::Application.config.secret_key_base = '77392af6c484f5689951c22f9cc59de6cf3d5bd1166ad5c0db3baf996cb528b3b625c1ff8faadc1c49a4ccb1621ab16a3285e8c3876bade2b8c65820ad72622a'
