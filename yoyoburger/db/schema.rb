82e9a0085c0f9ca42fbdb9744b5974814989657b
Oli Bates <olibates@live.com>
2014-02-12 12:51:06 +0000

    Static pages admin/controller added

--- yoyoburger/db/schema.rb
+++ yoyoburger/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140211123121) do
+ActiveRecord::Schema.define(version: 20140212124710) do
 
   create_table "spree_activators", force: true do |t|
     t.string   "description"
@@ -237,6 +237,27 @@ ActiveRecord::Schema.define(version: 20140211123121) do
   add_index "spree_orders", ["completed_at"], name: "index_spree_orders_on_completed_at"
   add_index "spree_orders", ["number"], name: "index_spree_orders_on_number"
   add_index "spree_orders", ["user_id"], name: "index_spree_orders_on_user_id"
+
+  create_table "spree_pages", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.string   "slug"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "show_in_header",           default: false, null: false
+    t.boolean  "show_in_footer",           default: false, null: false
+    t.string   "foreign_link"
+    t.integer  "position",                 default: 1,     null: false
+    t.boolean  "visible",                  default: true
+    t.string   "meta_keywords"
+    t.string   "meta_description"
+    t.string   "layout"
+    t.boolean  "show_in_sidebar",          default: false, null: false
+    t.string   "meta_title"
+    t.boolean  "render_layout_as_partial", default: false
+  end
+
+  add_index "spree_pages", ["slug"], name: "index_spree_pages_on_slug"
 
   create_table "spree_payment_methods", force: true do |t|
     t.string   "type"
