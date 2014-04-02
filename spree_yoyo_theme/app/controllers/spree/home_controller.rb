eec3e385a462eadc5710741fe8a0b196a03d8663
Laurie Steele <Laurie Steele>
2014-04-02 15:24:42 +0100

    google api distance

--- spree_yoyo_theme/app/controllers/spree/home_controller.rb
+++ spree_yoyo_theme/app/controllers/spree/home_controller.rb
@@ -12,14 +12,20 @@ module Spree
 
     def checkPostcode
     	@postcode = params[:postcode]
+      directions = GoogleDirections.new('BS8 1JT',@postcode)
     	if @postcode
         @postcode = @postcode.upcase
         if @postcode.match('^(((([A-PR-UWYZ][0-9][0-9A-HJKS-UW]?)|([A-PR-UWYZ][A-HK-Y][0-9][0-9ABEHMNPRV-Y]?))\s{0,2}[0-9]([ABD-HJLNP-UW-Z]{2}))|(GIR\s{0,2}0AA))$')
-	    	  flash[:success] = "Free delivery on orders over £10!"
+	    	  if directions.distance_in_miles<10
+          flash[:success] = "Free delivery on orders over £10!"
 	    	  redirect_to products_path
+          else
+          flash[:error] = "You are not within our radius"
+          redirect_to :back
+          end
 	      else
-	    	  flash[:error] = "Sorry, we don't deliver to you (or an invalid postcode!)"
-	    	  redirect_to :back
+	    	flash[:error] = "Invalid postcode"
+        redirect_to :back
 	      end
      end
     end
